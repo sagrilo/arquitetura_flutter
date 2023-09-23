63d53c0d5de48c969eb73eb5d03542085c318d01
sagrilo <wallace.sagrilo@gmail.com>
2023-09-23 17:09:29 -0300

    Criação da camada presentation

---
+++ lib/layers/presentation/ui/pages/carro_page.dart
@@ -0,0 +1,31 @@
+import 'package:arquitetura_padrao/layers/data/datasources/local/get_carros_por_cor_local_datasource_imp.dart';
+import 'package:arquitetura_padrao/layers/data/repositories/get_carro_por_cor_repository_imp.dart';
+import 'package:arquitetura_padrao/layers/data/repositories/salvar_carro_favorito_repository_imp.dart';
+import 'package:arquitetura_padrao/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_imp.dart';
+import 'package:arquitetura_padrao/layers/domain/usecases/salvar_carro_favorito/salvar_carro_favorito_usecase_imp.dart';
+import 'package:arquitetura_padrao/layers/presentation/controllers/carro_controller.dart';
+import 'package:flutter/material.dart';
+
+class CarroPage extends StatelessWidget {
+  CarroPage({Key? key}) : super(key: key);
+
+  CarroController controller = CarroController(
+      GetCarrosPorCorUseCaseImp(
+          GetCarrosPorCorRepositoryImp(
+              GetCarrosPorCorLocalDataSourceImp()
+          )
+      ),
+      SalvarCarroFavoritoUseCaseImp(
+          SalvarCarroFavoritoRepositoryImp()
+      )
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(
+      padding: EdgeInsets.all(24),
+      child: Text(controller.carro.placa)));
+
+  }
+
+}
