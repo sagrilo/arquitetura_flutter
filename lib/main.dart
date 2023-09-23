069cd2b681a62ec60af3229e45d372b75ce40295
sagrilo <wallace.sagrilo@gmail.com>
2023-09-23 17:56:35 -0300

    Injeção de dependência

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
-main() {
+import 'package:arquitetura_padrao/core/inject/inject.dart';
+import 'package:arquitetura_padrao/layers/presentation/ui/pages/carro_page.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  Inject.init();
+  runApp(
+      MaterialApp(
+        home: CarroPage(),
+      ),
+  );
 }
